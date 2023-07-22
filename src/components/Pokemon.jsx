cd601b50545ad21702a72dfc74b9b94c44f92e07
josh manansala <joshmanansalagsd@gmail.com>
2023-07-22 13:58:19 -0700

    add: loader img when page renders data

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useGlobalContext } from "../context/Global";
+import Loader from "./Loader";
 import "../styles/Pokemon.css";
 
 function Pokemon() {
@@ -22,6 +23,7 @@ function Pokemon() {
 
   useEffect(() => {
     getPokemon(name);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name]);
 
   useEffect(() => {
@@ -31,135 +33,142 @@ function Pokemon() {
   }, [pokemonItem]);
 
   return (
-    <div className="Pokemon">
-      <div className="back">
-        <Link to={"/"}>Back to Home</Link>
-      </div>
-      <div className="prev-next-pokemon-container">
-        <Link
-          style={{ display: previousPokemonId === 0 ? "none" : "" }}
-          className="previous-pokemon"
-          to={`/pokemon/${previousPokemonId}`}
-        >
-          Previous Pokemon
-        </Link>
-        <Link className="next-pokemon" to={`/pokemon/${id + 1}`}>
-          Next Pokemon
-        </Link>
-      </div>
-      <h2>{pokemonItem.name}</h2>
-      <div className="pokemon-card">
-        <div className="pokemon-card__image-container">
-          <img
-            src={sprites?.other["official-artwork"].front_default}
-            alt="pokemon-image"
-          />
-        </div>
-        <div className="pokemon-card__data-details">
-          <h3>Pokédex Data</h3>
-          <p>
-            <span>national no. </span>
-            <span>{id}</span>
-          </p>
-          <p className="pokemon-card__data-details__types">
-            <span>type</span>
-            {types?.map((type) => {
-              return (
-                <span
-                  style={{
-                    background:
-                      type.type.name === "grass"
-                        ? "#7c5"
-                        : type.type.name === "normal"
-                        ? "#aa9"
-                        : type.type.name === "fire"
-                        ? "#f42"
-                        : type.type.name === "water"
-                        ? "#39f"
-                        : type.type.name === "electric"
-                        ? "#fc3"
-                        : type.type.name === "ice"
-                        ? "#6cf"
-                        : type.type.name === "fighting"
-                        ? "#b54"
-                        : type.type.name === "poison"
-                        ? "#a59"
-                        : type.type.name === "ground"
-                        ? "#db5"
-                        : type.type.name === "flying"
-                        ? "#89f"
-                        : type.type.name === "psychic"
-                        ? "#f59"
-                        : type.type.name === "bug"
-                        ? "#ab2"
-                        : type.type.name === "rock"
-                        ? "#ba6"
-                        : type.type.name === "ghost"
-                        ? "#66b"
-                        : type.type.name === "dragon"
-                        ? "#76e"
-                        : type.type.name === "dark"
-                        ? "#754"
-                        : type.type.name === "steel"
-                        ? "#aab"
-                        : type.type.name === "fairy"
-                        ? "#e9e"
-                        : "white",
-                    margin: "0 .1rem",
-                    padding: ".5rem",
-                    borderRadius: "5px",
-                    textTransform: "capitalize",
-                    border: ".5px solid lightgray",
-                    width: "80px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  key={type.type.name}
-                >
-                  {type.type.name}
-                </span>
-              );
-            })}
-          </p>
-          <p className="species">
-            <span>species</span>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="Pokemon">
+          <div className="back">
+            <Link to={"/"}>Back to Home</Link>
+          </div>
+          <div className="prev-next-pokemon-container">
+            <Link
+              style={{ display: previousPokemonId === 0 ? "none" : "" }}
+              className="previous-pokemon"
+              to={`/pokemon/${previousPokemonId}`}
+            >
+              Previous Pokemon
+            </Link>
+            <Link className="next-pokemon" to={`/pokemon/${id + 1}`}>
+              Next Pokemon
+            </Link>
+          </div>
+          <h2>{pokemonItem.name}</h2>
+          <div className="pokemon-card">
+            <div className="pokemon-card__image-container">
+              <img
+                src={sprites?.other["official-artwork"].front_default}
+                alt="pokemon-image"
+              />
+            </div>
+            <div className="pokemon-card__data-details">
+              <h3>Pokédex Data</h3>
+              <p>
+                <span>national no. </span>
+                <span>{id}</span>
+              </p>
+              <p className="pokemon-card__data-details__types">
+                <span>type</span>
+                {types?.map((type) => {
+                  return (
+                    <span
+                      style={{
+                        background:
+                          type.type.name === "grass"
+                            ? "#7c5"
+                            : type.type.name === "normal"
+                            ? "#aa9"
+                            : type.type.name === "fire"
+                            ? "#f42"
+                            : type.type.name === "water"
+                            ? "#39f"
+                            : type.type.name === "electric"
+                            ? "#fc3"
+                            : type.type.name === "ice"
+                            ? "#6cf"
+                            : type.type.name === "fighting"
+                            ? "#b54"
+                            : type.type.name === "poison"
+                            ? "#a59"
+                            : type.type.name === "ground"
+                            ? "#db5"
+                            : type.type.name === "flying"
+                            ? "#89f"
+                            : type.type.name === "psychic"
+                            ? "#f59"
+                            : type.type.name === "bug"
+                            ? "#ab2"
+                            : type.type.name === "rock"
+                            ? "#ba6"
+                            : type.type.name === "ghost"
+                            ? "#66b"
+                            : type.type.name === "dragon"
+                            ? "#76e"
+                            : type.type.name === "dark"
+                            ? "#754"
+                            : type.type.name === "steel"
+                            ? "#aab"
+                            : type.type.name === "fairy"
+                            ? "#e9e"
+                            : "white",
+                        margin: "0 .1rem",
+                        padding: ".5rem",
+                        borderRadius: "5px",
+                        textTransform: "capitalize",
+                        border: ".5px solid lightgray",
+                        width: "80px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                      key={type.type.name}
+                    >
+                      {type.type.name}
+                    </span>
+                  );
+                })}
+              </p>
+              <p className="species">
+                <span>species</span>
 
-            {species.map((name) => {
-              return <span key={name.name}>{name.name}</span>;
-            })}
-          </p>
-          <p>
-            <span>Height</span>
-            <span className="height">
-              {height?.toString().slice(0, -1) +
-                "." +
-                height?.toString().slice(-1)}{" "}
-              m
-            </span>
-          </p>
-          <p>
-            <span>Weight</span>
-            <span className="weight">
-              {weight?.toString().slice(0, -1) +
-                "." +
-                weight?.toString().slice(-1)}{" "}
-              kg
-            </span>
-          </p>
-          <p className="abilities">
-            <span>Abilities</span>
-            {abilities?.map((ability) => {
-              return (
-                <span className="ability" key={ability.ability.name}>
-                  {ability.ability.name}
+                {species.map((name) => {
+                  return <span key={name.name}>{name.name}</span>;
+                })}
+              </p>
+              <p>
+                <span>Height</span>
+                <span>{height?.toString().length === 1 ? "0" : ""}</span>
+                <span className="height">
+                  {height?.toString().slice(0, -1) +
+                    "." +
+                    height?.toString().slice(-1)}{" "}
+                  m
                 </span>
-              );
-            })}
-          </p>
+              </p>
+              <p>
+                <span>Weight</span>
+                <span className="weight">
+                  {weight?.toString().slice(0, -1) +
+                    "." +
+                    weight?.toString().slice(-1)}{" "}
+                  kg
+                </span>
+              </p>
+              <p className="abilities">
+                <span>Abilities</span>
+                {abilities?.map((ability) => {
+                  return (
+                    <span className="ability" key={ability.ability.name}>
+                      {ability.ability.name}
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
