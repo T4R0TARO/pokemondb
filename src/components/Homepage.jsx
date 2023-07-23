f85561b2fadf68703bc9900887f2f4764d8769e6
josh manansala <joshmanansalagsd@gmail.com>
2023-07-23 14:11:16 -0700

    refactor: homepage mobile styles

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,30 +7,24 @@ function Homepage() {
   const { search, handleChange, handleSubmit, loading } = useGlobalContext();
 
   return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <main className="Homepage">
-          <form action="" className="search-form" onSubmit={handleSubmit}>
-            <div className="input-control">
-              <span>Name: </span>
-              <input
-                type="text"
-                placeholder="Search Pokemon"
-                value={search}
-                onChange={handleChange}
-              />
-              <button className="search-button" type="submit">
-                Search
-              </button>
-            </div>
-          </form>
-          <PokeSearchResults />
-          {/* {loading ? <Loader /> : <PokeSearchResults />} */}
-        </main>
-      )}
-    </>
+    <main className="Homepage">
+      <form action="" className="search-form" onSubmit={handleSubmit}>
+        <div className="input-control">
+          <span>Name: </span>
+          <input
+            type="text"
+            placeholder="Search Pokemon"
+            value={search}
+            onChange={handleChange}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+      {/* <PokeSearchResults /> */}
+      {loading ? <Loader /> : <PokeSearchResults />}
+    </main>
   );
 }
 
